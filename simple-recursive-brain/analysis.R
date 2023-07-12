a91413b2ba928913fbee735dcf8a6d684f2d626d
Ross Linscott <rossklin@gmail.com>
2023-07-12 22:33:44 +0200

    Update analysis script with a graph that uses the thread id and writes the output to a file

--- simple-recursive-brain/analysis.R
+++ simple-recursive-brain/analysis.R
@@ -3,9 +3,17 @@ library(readr)
 library(ggplot2)
 
 run <- function() {
-    df <- read_csv("simple-recursive-brain/output.csv") %>% 
-        mutate(test_id = as.character(test_id))
+    df <- read_csv("simple-recursive-brain/output.csv") %>%
+        mutate(test_id = as.integer(test_id), thread_id = as.integer(thread_id)) %>%
+        group_by(thread_id) %>%
+        mutate(is_max_test_id = test_id == max(test_id)) %>%
+        ungroup()
 
-    ggplot(df, aes(x = iteration, y = test, group = test_id, color = test_id)) + 
-        geom_smooth(se = F)
+    tmax <- max(df$iteration)
+
+    ggplot(data = df) +
+        geom_path(aes(x = iteration, y = rolling_rate, group = test_id, color = as.factor(thread_id), alpha = 0.2 + 0.8 * as.integer(is_max_test_id))) +
+        geom_path(data = tibble(x = c(0, tmax), y = c(0.25, 0.25 + 0.00002 * tmax)), aes(x = x, y = y))
+
+    ggsave(filename = "res.png", width = 16)
 }
