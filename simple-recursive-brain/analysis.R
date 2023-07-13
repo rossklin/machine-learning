a4950c03ca6b85354c5e4162b063e376537a0156
Ross Linscott <rossklin@gmail.com>
2023-07-13 13:20:18 +0200

    SRB: analysis.R: run forever and update the limit function to match

--- simple-recursive-brain/analysis.R
+++ simple-recursive-brain/analysis.R
@@ -3,17 +3,24 @@ library(readr)
 library(ggplot2)
 
 run <- function() {
-    df <- read_csv("simple-recursive-brain/output.csv") %>%
-        mutate(test_id = as.integer(test_id), thread_id = as.integer(thread_id)) %>%
-        group_by(thread_id) %>%
-        mutate(is_max_test_id = test_id == max(test_id)) %>%
-        ungroup()
+    while (T) {
+        df <- read_csv("simple-recursive-brain/output.csv") %>%
+            mutate(test_id = as.integer(test_id), thread_id = as.integer(thread_id)) %>%
+            group_by(thread_id) %>%
+            mutate(is_max_test_id = test_id == max(test_id)) %>%
+            ungroup()
 
-    tmax <- max(df$iteration)
+        tmax <- max(df$iteration)
+        ts <- 0:tmax
+        limits <- 1 / (1 + exp(-ts / 2200)) - 0.357
 
-    ggplot(data = df) +
-        geom_path(aes(x = iteration, y = rolling_rate, group = test_id, color = as.factor(thread_id), alpha = 0.2 + 0.8 * as.integer(is_max_test_id))) +
-        geom_path(data = tibble(x = c(0, tmax), y = c(0.25, 0.25 + 0.00002 * tmax)), aes(x = x, y = y))
+        ggplot() +
+            geom_path(data = df %>% filter(is_max_test_id), alpha = 1, aes(x = iteration, y = rolling_rate, group = test_id, color = as.factor(thread_id))) +
+            geom_path(data = df %>% filter(!is_max_test_id), alpha = 0.2, aes(x = iteration, y = rolling_rate, group = test_id, color = as.factor(thread_id))) +
+            geom_path(data = tibble(x = ts, y = limits), aes(x = x, y = y))
 
-    ggsave(filename = "res.png", width = 16)
+        ggsave(filename = "res.png", width = 16)
+
+        Sys.sleep(30)
+    }
 }
