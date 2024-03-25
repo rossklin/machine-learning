8ed8d64efdda5935940cafbaa9a678cea407fca7
Ross Linscott <rossklin@gmail.com>
2024-03-25 20:59:47 +0100

    SRB node.cpp implement last_fired_before and did_fire_at

--- simple-recursive-brain/src/node.cpp
+++ simple-recursive-brain/src/node.cpp
@@ -14,3 +14,44 @@ std::set<time_point> Node::fired_at_set() const
 {
     return std::set<time_point>(fired_at.begin(), fired_at.end());
 }
+
+// Find the latest time_point in the ordered vector fired_at that is no later than t
+time_point Node::last_fired_before(time_point t) const
+{
+    if (fired_at.empty())
+    {
+        return -1;
+    }
+
+    int top = fired_at.size() - 1; // Highest possible index
+    int bottom = 0;                // Lowest possible index
+
+    while (top > bottom)
+    {
+        int idx = bottom + (top - bottom + 1) / 2; // idx guaranteed greater than bottom
+        if (fired_at[idx] <= t)
+        {
+            bottom = idx;
+        }
+        else
+        {
+            top = idx - 1;
+        }
+    }
+
+    time_point t2 = fired_at[bottom];
+
+    if (t2 <= t)
+    {
+        return t2;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+bool Node::did_fire_at(time_point t) const
+{
+    return last_fired_before(t) == t;
+};
